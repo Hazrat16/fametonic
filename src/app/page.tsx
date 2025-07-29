3f3b1b4a07fcf65126a880efc17caef40ef1976f
hazrat <hazrat17016@gmail.com>
2025-07-29 22:50:39 +0600

    Remove unnecessary comments and adjust spacing in home page layout for cleaner code and improved readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
       className="min-h-screen flex flex-col-reverse md:flex-row bg-no-repeat bg-[length:100%] md:bg-[length:62%_90%] bg-[position:top_center] md:bg-[position:100%_0%] overflow-x-hidden "
       style={{ backgroundImage: 'url("/images/bg-image.png")' }}
     >
-      {/* Left Text Side */}
       <div className="w-full md:w-1/2 flex flex-col justify-center px-6 md:px-[0px] pt-28 md:pt-0">
         <h1 className="font-urbanist font-extrabold md:font-bold text-[25px] md:text-[35px] leading-[100%] text-white mb-4">
           Want to Turn Social Media Into a <br className="hidden md:block" />
@@ -18,7 +17,6 @@ export default function Home() {
           Fametonic:
         </h2>
 
-        {/* Feature Points */}
         <div className="space-y-2 mb-6">
           {[
             "Start growing your influence right away—no waiting required!",
@@ -36,11 +34,10 @@ export default function Home() {
         </div>
 
         <div className="flex flex-col-reverse md:flex-col">
-          {/* Button Section - should appear last in mobile, first in desktop */}
           <div className="w-full md:w-1/2 mb-6 md:mb-0">
-            <button className="w-full h-[40px] rounded-[10px] bg-[#FC004E] text-white font-figtree font-bold text-[20px] px-10 py-2 shadow-[2px_2px_10px_#00E7F9] mb-2 flex items-center justify-center gap-2">
+            <button className="w-full h-[40px] rounded-[10px] bg-[#FC004E] text-white font-figtree font-bold text-[20px] px-10 py-2 shadow-[2px_2px_10px_#00E7F9] mb-2 flex items-center justify-center gap-0.5">
               Get Started
-              <ForwardButtonIcon height="8" width="14" />
+              <ForwardButtonIcon height="10" width="20" />
             </button>
 
             <p className="font-figtree font-normal text-[12px] leading-[16px] text-center text-white">
@@ -48,11 +45,11 @@ export default function Home() {
             </p>
           </div>
 
-          {/* Text Sections */}
           <div className="flex flex-col md:flex-col items-center text-center md:text-start md:items-start">
-            <p className="text-[12px] leading-[22px] text-[#ABABAB] my-[30px] font-medium">
+            <p className="text-[12px] leading-[22px] text-[#ABABAB] my-[20px] font-medium">
               By clicking &quot;Get Started&quot;, you agree with Terms and
-              Conditions, Privacy Policy, Subscription Terms
+              Conditions, Privacy Policy, Subscription <br />
+              Terms
             </p>
 
             <p className="text-[10px] leading-[22px] font-medium text-[#ABABAB] mb-6 md:mb-[30px]">
@@ -62,7 +59,6 @@ export default function Home() {
         </div>
       </div>
 
-      {/* Image container (empty on purpose to display bg-image) */}
       <div className="w-full md:w-1/2 h-[250px] md:h-auto"></div>
     </div>
   );
